d87aff0f3481badd9f94e92a4353c6791142d893
Hatala Mark <hatala.mark@gmail.com>
2023-07-07 16:53:17 +0200

    authSlice added

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,18 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+const initialState = {
+  user: null,
+  isError: false,
+  isSuccess: false,
+  isLOading: false,
+  message: '',
+}
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {},
+})
+
+export default authSlice.reducer
