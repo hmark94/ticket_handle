553e204c952dd08df5f372628ef99eb855bb28f4
Hatala Mark <hatala.mark@gmail.com>
2023-07-28 18:18:25 +0200

    bugs fixed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
-const colors = require('colors')
-const dotenv = require('dotenv').config()
+require('colors')
+require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
@@ -23,12 +23,13 @@ if (process.env.NODE_ENV === 'production') {
   // Set build folder as static
   app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-  app.get('*', (req, res) =>
-    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
-  )
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  })
 } else {
-  app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to the support desk API' })
+  app.get('/', (_, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' })
   })
 }
 
